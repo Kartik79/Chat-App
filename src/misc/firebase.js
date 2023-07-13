5cbc309f28e5354fb9e20a3e9cb8c326cae79057
Kartik Tiwari <tiwarikartik79@gmail.com>
2023-07-13 19:46:15 +0530

    initialized firebase project

---
+++ src/misc/firebase.js
@@ -0,0 +1,12 @@
+import firebase from 'firebase/app'
+
+const config ={
+    apiKey: "AIzaSyBzOW2vyqFbUQKzanYNAWadlMcgO9LhM-o",
+    authDomain: "chat-app-839d7.firebaseapp.com",
+    projectId: "chat-app-839d7",
+    storageBucket: "chat-app-839d7.appspot.com",
+    messagingSenderId: "1015343742135",
+    appId: "1:1015343742135:web:97f661adb0cab6a0be6132"
+  };
+
+  firebase.intializeApp(config)
