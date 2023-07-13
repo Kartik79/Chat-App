a73cfb83d9a33c7a9e6b5a104a3abfa714b3c389
Kartik Tiwari <tiwarikartik79@gmail.com>
2023-07-13 17:48:25 +0530

    sacffolding

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React from "react";
+import 'rsuite/dist/rsuite.min.css';
+import './styles/main.scss';
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
