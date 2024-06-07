cf57c8979c4f1a1d1045658598139409e6a33fdf
Kartik Tiwari <tiwarikartik79@gmail.com>
2024-06-07 18:03:15 +0530

    Updated nickname in database

--- chat-app/src/components/dashboard/index.js
+++ chat-app/src/components/dashboard/index.js
@@ -1,10 +1,24 @@
 import React from 'react';
-import { Drawer, Button } from 'rsuite';
+import { Drawer, Button, Divider, Alert } from 'rsuite';
 import { useProfile } from '../../context/profile.context';
+import EditableInput from '../EditableInput';
+import { database } from '../../misc/firebase';
 
 function Dashboard({ onSignOut }) {
   const { profile } = useProfile();
+  const onSave = async newData => {
+    const userNicknameRef = database
+      .ref(`/profiles/${profile.uid}`)
+      .child('name');
 
+    try {
+      await userNicknameRef.set(newData);
+
+      Alert.success('Nickname has been updated', 4000);
+    } catch (err) {
+      Alert.error(err.message, 4000);
+    }
+  };
   return (
     <>
       <Drawer.Header>
@@ -13,6 +27,13 @@ function Dashboard({ onSignOut }) {
 
       <Drawer.Body>
         <h3>Hey, {profile.name}</h3>
+        <Divider />
+        <EditableInput
+          name="nickname"
+          initialValue={profile.name}
+          onSave={onSave}
+          label={<h6 className="mb-2">Nickname</h6>}
+        />
       </Drawer.Body>
 
       <Drawer.Footer>
