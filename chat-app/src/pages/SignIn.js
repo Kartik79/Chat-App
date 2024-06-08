b61bf5800ac20ca025aa2ae770707aea60154942
Kartik Tiwari <tiwarikartik79@gmail.com>
2024-06-08 16:23:49 +0530

    add create room button and created chat room

--- chat-app/src/pages/SignIn.js
+++ chat-app/src/pages/SignIn.js
@@ -41,11 +41,11 @@ function SignIn() {
     
                   <div className="mt-3">
                     <Button block color="blue" onClick={onFacebookSignIn}>
-                    <Icon icon="facebook" />Continue with Facebook
+                    <Icon icon="facebook" /> Continue with Facebook
                     </Button>
     
                     <Button block color="green" onClick={onGoogleSignIn}>
-                    <Icon icon="google" />Continue with Google
+                    <Icon icon="google" /> Continue with Google
                     </Button>
                   </div>
                 </Panel>
