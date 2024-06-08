5ee76455146fab68b617c446cba06f7032aaabf6
Kartik Tiwari <tiwarikartik79@gmail.com>
2024-06-08 17:06:03 +0530

    created layout for chat page

---
+++ chat-app/src/components/chat-window/bottom/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Bottom() {
+  return <div>messages</div>;
+}
+
+export default Bottom;
---
+++ chat-app/src/components/chat-window/top/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Top() {
+  return <div>messages</div>;
+}
+
+export default Top;
